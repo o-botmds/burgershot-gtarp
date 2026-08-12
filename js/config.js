fea7e36eb3a2a4037b3e7fba44c7b4d6a141e8ae
o-botmds <botmds14@gmail.com>
2026-08-12 22:48:07 +0000

    commit

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Configuração do Supabase
-const SUPABASE_URL = 'https://SEU_PROJETO.supabase.co';
-const SUPABASE_KEY = 'SUA_CHAVE_PUBLICA';
+const SUPABASE_URL = 'https://xjxofnruajzoououzyeo.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_fac88LXDfbr3CFcyvSHlpA_7BVmCkyc';
 
 // Criar cliente Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
